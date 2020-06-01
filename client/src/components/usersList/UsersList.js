5e9b755f94638872dabd1f21893d43817e4c792a
priyankamadhwal <priyanka0701m@gmail.com>
2020-06-02 02:03:37 +0530

    adding online users list on dashboard

--- client/src/components/usersList/UsersList.js
+++ client/src/components/usersList/UsersList.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Icon from "@material-ui/core/Icon";
 
 import "./UsersList.css";
 
 const UsersList = ({ onlineUsers }) => {
   return (
-    <div className='usersList'>
-      <h1>Users list</h1>
-      <div>
+    <div className='usersListOuterContainer'>
+      <span className='usersListHeading'>
+        Curently available in this channel
+      </span>
+      <div className='usersListInnerContainer'>
         {onlineUsers.users
-          ? onlineUsers.users.map((user, i) => <p key={i}>{user.username}</p>)
+          ? onlineUsers.users.map((user, i) => (
+              <div key={i} className='usersListOnlineUserInfo'>
+                <Icon className='fas fa-circle' />
+                <span>{user.username}</span>
+              </div>
+            ))
           : null}
       </div>
     </div>
