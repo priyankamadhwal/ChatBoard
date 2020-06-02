76932ff8075432b435d2b71cecf21a3e1f8e4fc1
priyankamadhwal <priyanka0701m@gmail.com>
2020-06-03 01:25:44 +0530

    adding welcome page

--- client/src/components/welcome/Welcome.js
+++ client/src/components/welcome/Welcome.js
@@ -1,9 +1,18 @@
 import React from "react";
+import ReactEmoji from "react-emoji";
 
 import "./Welcome.css";
 
 const Welcome = (props) => {
-  return <div className='welcomeOuterContainer'>Hey there!</div>;
+  return (
+    <div className='welcomeOuterContainer'>
+      <h1>Welcome!</h1>
+      <p className='intro'>
+        Don't miss out on the good stuff when you are just one chat away from
+        your favorite topics {ReactEmoji.emojify("<3")}
+      </p>
+    </div>
+  );
 };
 
 export default Welcome;
